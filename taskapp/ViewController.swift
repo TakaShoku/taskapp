c76944d35b2f519a613d1413878020a6cf145de2
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-07-28 21:10:35 +0900

    [add] 画面遷移する時にデータを渡す

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -12,6 +12,7 @@ import RealmSwift   // ←追加
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var tableView: UITableView!
+    var task: Task!
     
     // Realmインスタンスを取得する
     let realm = try! Realm()  // ←追加
@@ -77,6 +78,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 self.realm.delete(self.taskArray[indexPath.row])
                 tableView.deleteRows(at: [indexPath], with: .fade)
             }
+        }
+    }
+    
+    // segue で画面遷移するに呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            task.date = Date()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
         }
     }
 }

6d4065351463888fcaf307dcb8807f534208d315
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-08-04 22:33:36 +0900

    [add] UISearchBarでキャンセルボタンの表示・非表示を切り替

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         
         searchBar.delegate = self as UISearchBarDelegate
-        searchBar.showsCancelButton = true
+        
         //プレースホルダの指定
         searchBar.placeholder = "検索"
         
@@ -49,7 +49,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         print("検索完了")
     }
     
+    // キーボードが表示を監視
+    override func viewWillAppear(_ animated: Bool) {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: Selector(("handleKeyboardWillShowNotification:")), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    }
+    
+    // キーボードが表示されるときにキャンセルボタンを有効に
+    func handleKeyboardWillShowNotification(notification: NSNotification) {
+        searchBar.showsCancelButton = true
+    }
+    
     func searBarCancelButtonClicked(_ seachBar: UISearchBar) {
+        // キャンセルボタンが押されたらキャンセルボタンを無効にしてフォーカスをはずす
+        searchBar.showsCancelButton = false
+        searchBar.resignFirstResponder()
         print("キャンセル完了")
     }
     
@@ -139,9 +153,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tableView.reloadData()
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        tableView.reloadData()
+//    }
 }
 
